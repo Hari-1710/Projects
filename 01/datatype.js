abde19b30512789ce77c645afef6210e58f6be88
Hari-1710 <146413094+Hari-1710@users.noreply.github.com>
2025-07-08 17:04:54 +0000

    datatypes

---
+++ 01/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 ) we are using nodejs, not browser
+
+console.log(3 + 2)
+console.log("hari")
+
+let name = "hari"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//types of datatype
+/*
+number
+bigint
+string
+boolean
+null = standalone value
+undefined 
+symbol = unique
+*/
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
