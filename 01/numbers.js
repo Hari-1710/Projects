c314344cc2d6a0c9ee2dc16d0f6a1e3c4d34f373
Hari-1710 <harikrishnanmylatty1234@gmail.com>
2025-07-10 11:57:10 +0530

    math

--- 01/numbers.js
+++ 01/numbers.js
@@ -1,13 +1,26 @@
-const score = 400
-console.log(score);
+// const score = 400
+// console.log(score);
 
-const balance = new Number(1000);
-console.log(balance);
+// const balance = new Number(1000);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
-const otherNumber = 23.8966
+// const otherNumber = 23.8966
 
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
 
+//**********maths */
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.ceil(4.5)); // 5
+// console.log(Math.floor(4.5)); // 4
+// console.log(Math.round(4.5)); // 5
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
